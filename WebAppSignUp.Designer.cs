97200fa462dc8647a61626c5328a9def2adf0fcb
ALGER TITA JOY BAJO <a.bajo.548968umindanao.edu.ph>
2025-09-10 23:03:16 +0800

    web-app

---
+++ WebAppSignUp.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace FlavorFlowIT13
+{
+    partial class WebAppSignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            signuppanel = new Panel();
+            webapploginbtn = new Button();
+            webappsignupbtn = new Button();
+            webappaddresstxt = new TextBox();
+            webapppasswordtxt = new TextBox();
+            webappconfirmpasswordtxt = new TextBox();
+            webappphonenumbertxt = new TextBox();
+            webappemailaddresstxt = new TextBox();
+            webappnametxt = new TextBox();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            name = new Label();
+            signuppanel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // signuppanel
+            // 
+            signuppanel.BackColor = Color.Transparent;
+            signuppanel.Controls.Add(webapploginbtn);
+            signuppanel.Controls.Add(webappsignupbtn);
+            signuppanel.Controls.Add(webappaddresstxt);
+            signuppanel.Controls.Add(webapppasswordtxt);
+            signuppanel.Controls.Add(webappconfirmpasswordtxt);
+            signuppanel.Controls.Add(webappphonenumbertxt);
+            signuppanel.Controls.Add(webappemailaddresstxt);
+            signuppanel.Controls.Add(webappnametxt);
+            signuppanel.Controls.Add(label5);
+            signuppanel.Controls.Add(label4);
+            signuppanel.Controls.Add(label3);
+            signuppanel.Controls.Add(label2);
+            signuppanel.Controls.Add(label1);
+            signuppanel.Controls.Add(name);
+            signuppanel.Location = new Point(318, 106);
+            signuppanel.Name = "signuppanel";
+            signuppanel.Size = new Size(732, 583);
+            signuppanel.TabIndex = 1;
+            // 
+            // webapploginbtn
+            // 
+            webapploginbtn.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            webapploginbtn.Location = new Point(238, 489);
+            webapploginbtn.Name = "webapploginbtn";
+            webapploginbtn.Size = new Size(339, 63);
+            webapploginbtn.TabIndex = 13;
+            webapploginbtn.Text = "Log In";
+            webapploginbtn.UseVisualStyleBackColor = true;
+            // 
+            // webappsignupbtn
+            // 
+            webappsignupbtn.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            webappsignupbtn.Location = new Point(238, 409);
+            webappsignupbtn.Name = "webappsignupbtn";
+            webappsignupbtn.Size = new Size(339, 63);
+            webappsignupbtn.TabIndex = 12;
+            webappsignupbtn.Text = "Sign Up";
+            webappsignupbtn.UseVisualStyleBackColor = true;
+            // 
+            // webappaddresstxt
+            // 
+            webappaddresstxt.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            webappaddresstxt.Location = new Point(238, 69);
+            webappaddresstxt.Multiline = true;
+            webappaddresstxt.Name = "webappaddresstxt";
+            webappaddresstxt.Size = new Size(339, 42);
+            webappaddresstxt.TabIndex = 11;
+            // 
+            // webapppasswordtxt
+            // 
+            webapppasswordtxt.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            webapppasswordtxt.Location = new Point(238, 273);
+            webapppasswordtxt.Multiline = true;
+            webapppasswordtxt.Name = "webapppasswordtxt";
+            webapppasswordtxt.Size = new Size(339, 42);
+            webapppasswordtxt.TabIndex = 10;
+            // 
+            // webappconfirmpasswordtxt
+            // 
+            webappconfirmpasswordtxt.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            webappconfirmpasswordtxt.Location = new Point(238, 339);
+            webappconfirmpasswordtxt.Multiline = true;
+            webappconfirmpasswordtxt.Name = "webappconfirmpasswordtxt";
+            webappconfirmpasswordtxt.Size = new Size(339, 42);
+            webappconfirmpasswordtxt.TabIndex = 9;
+            // 
+            // webappphonenumbertxt
+            // 
+            webappphonenumbertxt.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            webappphonenumbertxt.Location = new Point(238, 132);
+            webappphonenumbertxt.Multiline = true;
+            webappphonenumbertxt.Name = "webappphonenumbertxt";
+            webappphonenumbertxt.Size = new Size(339, 42);
+            webappphonenumbertxt.TabIndex = 8;
+            // 
+            // webappemailaddresstxt
+            // 
+            webappemailaddresstxt.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            webappemailaddresstxt.Location = new Point(238, 201);
+            webappemailaddresstxt.Multiline = true;
+            webappemailaddresstxt.Name = "webappemailaddresstxt";
+            webappemailaddresstxt.Size = new Size(339, 42);
+            webappemailaddresstxt.TabIndex = 7;
+            // 
+            // webappnametxt
+            // 
+            webappnametxt.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            webappnametxt.Location = new Point(238, 6);
+            webappnametxt.Multiline = true;
+            webappnametxt.Name = "webappnametxt";
+            webappnametxt.Size = new Size(339, 42);
+            webappnametxt.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            label5.ForeColor = Color.SandyBrown;
+            label5.Location = new Point(35, 351);
+            label5.Name = "label5";
+            label5.Size = new Size(191, 30);
+            label5.TabIndex = 5;
+            label5.Text = "Confirm Password:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            label4.ForeColor = Color.SandyBrown;
+            label4.Location = new Point(66, 144);
+            label4.Name = "label4";
+            label4.Size = new Size(160, 30);
+            label4.TabIndex = 4;
+            label4.Text = "Phone number:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            label3.ForeColor = Color.SandyBrown;
+            label3.Location = new Point(74, 213);
+            label3.Name = "label3";
+            label3.Size = new Size(152, 30);
+            label3.TabIndex = 3;
+            label3.Text = "Email Address:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            label2.ForeColor = Color.SandyBrown;
+            label2.Location = new Point(118, 285);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 30);
+            label2.TabIndex = 2;
+            label2.Text = "Password:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            label1.ForeColor = Color.SandyBrown;
+            label1.Location = new Point(131, 81);
+            label1.Name = "label1";
+            label1.Size = new Size(95, 30);
+            label1.TabIndex = 1;
+            label1.Text = "Address:";
+            // 
+            // name
+            // 
+            name.AutoSize = true;
+            name.CausesValidation = false;
+            name.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            name.ForeColor = Color.SandyBrown;
+            name.Location = new Point(151, 18);
+            name.Name = "name";
+            name.Size = new Size(75, 30);
+            name.TabIndex = 0;
+            name.Text = "Name:";
+            // 
+            // WebAppSignUp
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1370, 749);
+            Controls.Add(signuppanel);
+            Name = "WebAppSignUp";
+            Text = "WebAppSignUp";
+            Load += WebAppSignUp_Load;
+            signuppanel.ResumeLayout(false);
+            signuppanel.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel signuppanel;
+        private Button webapploginbtn;
+        private Button webappsignupbtn;
+        private TextBox webappaddresstxt;
+        private TextBox webapppasswordtxt;
+        private TextBox webappconfirmpasswordtxt;
+        private TextBox webappphonenumbertxt;
+        private TextBox webappemailaddresstxt;
+        private TextBox webappnametxt;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Label name;
+    }
+}
